3e846058107d3bfeed60501a318db96b79b8d900
Fahamidul Hasan <fahimxt@gmail.com>
2026-08-10 15:51:41 +0600

    Update dual NAT gateway architecture for Terraform and Ansible

--- terraform-aws-test/main.tf
+++ terraform-aws-test/main.tf
@@ -83,20 +83,39 @@ resource "aws_subnet" "private_b" {
 
 # ---------------- NAT ----------------
 
-resource "aws_eip" "nat_eip" {
+resource "aws_eip" "nat_eip_a" {
   domain = "vpc"
 
   tags = {
-    Name = "${var.project_name}-nat-eip"
+    Name = "${var.project_name}-nat-eip-a"
   }
 }
 
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat_eip.id
+resource "aws_eip" "nat_eip_b" {
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip-b"
+  }
+}
+
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_eip_a.id
   subnet_id     = aws_subnet.public_a.id
 
   tags = {
-    Name = "${var.project_name}-nat"
+    Name = "${var.project_name}-nat-a"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat_b" {
+  allocation_id = aws_eip.nat_eip_b.id
+  subnet_id     = aws_subnet.public_b.id
+
+  tags = {
+    Name = "${var.project_name}-nat-b"
   }
 
   depends_on = [aws_internet_gateway.igw]
@@ -127,27 +146,40 @@ resource "aws_route_table_association" "public_b_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_route_table" "private_rt" {
+resource "aws_route_table" "private_rt_a" {
   vpc_id = aws_vpc.main.id
 
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat.id
+    nat_gateway_id = aws_nat_gateway.nat_a.id
   }
 
   tags = {
-    Name = "${var.project_name}-private-rt"
+    Name = "${var.project_name}-private-rt-a"
+  }
+}
+
+resource "aws_route_table" "private_rt_b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt-b"
   }
 }
 
 resource "aws_route_table_association" "private_a_assoc" {
   subnet_id      = aws_subnet.private_a.id
-  route_table_id = aws_route_table.private_rt.id
+  route_table_id = aws_route_table.private_rt_a.id
 }
 
 resource "aws_route_table_association" "private_b_assoc" {
   subnet_id      = aws_subnet.private_b.id
-  route_table_id = aws_route_table.private_rt.id
+  route_table_id = aws_route_table.private_rt_b.id
 }
 
 # ---------------- Security Groups ----------------
