cc3134ebaff36d18d5e96c6a096e58662a6a2814
Fahamidul Hasan <fahimxt@gmail.com>
2026-08-16 12:45:54 +0600

    Add ASG capacity wait timeout for Terraform

--- terraform-aws-test/main.tf
+++ terraform-aws-test/main.tf
@@ -373,6 +373,8 @@ resource "aws_autoscaling_group" "app_asg" {
 
   health_check_type         = "ELB"
   health_check_grace_period = 300
+
+  wait_for_capacity_timeout = "20m"
 }
 
 # ---------------- RDS ----------------
